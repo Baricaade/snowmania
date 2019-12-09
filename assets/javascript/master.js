bdbb181dad3ec042bd56dd689adb91abffe5deb5
Baricaade <baricaade@gmail.com>
2019-12-08 21:54:25 -0800

    Finished about section

--- assets/javascript/master.js
+++ assets/javascript/master.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {
   const nav = document.querySelector('.navbar');
   const scroll = document.documentElement.scrollTop
-  if (scroll > 100) {
+  if (scroll > 225) {
     nav.classList.add('navScroll');
   } else {
     nav.classList.remove('navScroll');
